56946abc7ea65256634e55a974d4f30d94190f05
Ed Gonzales <edgonzales6@gmail.com>
2023-12-04 11:27:14 -0800

    exercise-01 complete

--- exercises.py
+++ exercises.py
@@ -10,8 +10,13 @@
 
 # Hints:  Use the in operator to check if a character is in another string
 #         For example, if some_char in 'abc':
+vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"]
 
-
+letter = input('Please enter a letter from the alphabet (a-z or A-Z): ')
+if letter in vowels:
+    print(f'The letter "{letter}" is a vowel')
+else:
+    print(f'The letter "{letter}" is a consonant')
 
 
 
